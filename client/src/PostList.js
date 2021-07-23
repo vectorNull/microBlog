f4c2e6fc614102eff6915109aef37ba4be6cb322
vectorNull <jose@vectornullsec.com>
2021-07-22 20:31:46 -0700

    Add bootstrap styling

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -27,7 +27,7 @@ export default () => {
         );
     });
 
-    return <div>
+    return <div className="d-flex flex-row flex-wrap justify-content-between">
         {renderedPosts}
     </div>
 };
