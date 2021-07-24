8991b0e8540189a8b62e575b8918fc5eba0f8b42
vectorNull <jose@vectornullsec.com>
2021-07-24 14:07:44 -0700

    Add error handling

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,20 +1,33 @@
-const express = require('express')
-const axios = require('axios')
+const express = require("express");
+const axios = require("axios");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.post('/events', (req, res) => {
-    const event = req.body;
-
-    // Assumption: all of these are going to succeed
-    axios.post('http://localhost:4000/events', event)
-    axios.post("http://localhost:4001/events", event);
-    axios.post("http://localhost:4002/events", event);
-
-    res.send({ status: 'OK' })
+process.on("uncaughtException", (err) => {
+    console.log(err);
 })
+
+app.post("/events", (req, res) => {
+    console.log(req.body);
+    const event = req.body;
+    // Assumption: all of these are going to succeed
+    // Post Service
+    axios.post("http://localhost:4000/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    // Comments Service
+    axios.post("http://localhost:4001/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    // Query Service
+    axios.post("http://localhost:4002/events", event).catch((err) => {
+        console.log(err.message);
+    });
+
+    res.send({ status: "OK" });
+});
 
 app.listen(4005, () => {
-    console.log('Event Bus listening on 4005');
-})
+    console.log("Event Bus listening on 4005");
+});
