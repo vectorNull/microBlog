ce584937519ec37dbbe4d28309586ef632687784
vectorNull <jose@vectornullsec.com>
2021-07-25 11:28:26 -0700

    Add event route and status logic

--- moderation/index.js
+++ moderation/index.js
@@ -4,8 +4,26 @@ const axios = require('axios')
 const app = express()
 app.use(express.json())
 
-app.post('/events', (req, res) => {
+process.on("uncaughtException", (err) => {
+    console.log(err);
+});
 
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved'
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        })
+    }
+    res.send({});
 })
 
 app.listen(4003, () => {
