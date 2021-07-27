61d8dc653cf889183a388fd2ab321c29de6ab2d9
vectorNull <jose@vectornullsec.com>
2021-07-27 12:08:51 -0700

    Update url to clusterIP services

--- event-bus/index.js
+++ event-bus/index.js
@@ -20,18 +20,18 @@ app.post("/events", (req, res) => {
     axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
         console.log(err.message);
     });
-    // // Comments Service
-    // axios.post("http://localhost:4001/events", event).catch((err) => {
-    //     console.log(err.message);
-    // });
-    // // Query Service
-    // axios.post("http://localhost:4002/events", event).catch((err) => {
-    //     console.log(err.message);
-    // });
-    // // Moderation Service
-    // axios.post("http://localhost:4003/events", event).catch((err) => {
-    //     console.log(err.message);
-    // });
+    // Comments Service
+    axios.post("http://comments-srv:4001/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    // Query Service
+    axios.post("http://query-srv:4002/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    // Moderation Service
+    axios.post("http://moderation-srv:4003/events", event).catch((err) => {
+        console.log(err.message);
+    });
 
     res.send({ status: "OK" });
 });
